90b7c91c7820ba2890b8b52f28eac5560e1c4557
jabba69baba <129668615+jabba69baba@users.noreply.github.com>
2026-08-08 12:58:01 +0530

    Update PlayerScreen.kt

--- app/src/main/java/com/example/minimusic/ui/screens/PlayerScreen.kt
+++ app/src/main/java/com/example/minimusic/ui/screens/PlayerScreen.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -120,6 +123,7 @@ fun PlayerScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.statusBars)
             .padding(horizontal = 20.dp)
             .padding(top = 4.dp, bottom = 12.dp)
     ) {
